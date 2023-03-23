abf0bd55351a77e6a3bec3d2dd06dbccf07ac66a
Kuba Milcarz <kubamilcarz@yahoo.com>
2023-03-23 14:19:50 +0100

    Initial Commit

---
+++ Earth Heroes/Earth_HeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Earth_HeroesApp.swift
+//  Earth Heroes
+//
+//  Created by Kuba Milcarz on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Earth_HeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
